7479b0f4e40f006ee25470336a4b9b533ebc8fde
aaronphilip <aaronpphilip@gmail.com>
2019-12-27 08:41:55 -0800

    format changes and post 1

--- _assets/javascripts/webfonts.js
+++ _assets/javascripts/webfonts.js
@@ -2,6 +2,6 @@
 
 WebFont.load({
   google: {
-    families: ['Cormorant Garamond:700', 'Lato:300,400,700']
+    families: ['Cormorant Garamond:700', 'Lato:300,400,700', 'Crimson Text:700,400', 'Libre Baskerville:700,600,500,400,300']
   }
 });
---
+++ _my_tags/prayers.md
@@ -0,0 +1,4 @@
+---
+slug: prayers
+name: Prayers
+---
